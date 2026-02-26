a3355e7394f9f29168ab590f40664a788d0d7bfc
hannnmstfa <burhanmusthofa1@gmail.com>
2026-02-27 02:30:51 +0700

    new update iklan

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -8,7 +8,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3683911400321928"
+     crossorigin="anonymous"></script>
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/css/font.css', 'resources/js/app.js'])
 </head>
